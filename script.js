01446587b618f0c39f3cd7e007f8bd7a07cdcd47
LOGFORCE <105729788+logforce@users.noreply.github.com>
2023-07-03 17:29:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ document.addEventListener('DOMContentLoaded', function () {
       case "hi":
       case "hola":
         clearInput();
-        addTextToResults("Hello, I am LOGFORCE™ and at the moment I am under intensive training to enhance my capabilities and become even more advanced. According to my developers, the new version of me will be released soon and I will be ready to serve you to the best of my abilities.");
+        addTextToResults("Hello, I am SynA and at the moment I am under intensive training to enhance my capabilities and become even more advanced. According to my developers, the new version of me 👽 will be released soon and I will be ready to serve you to the best of my abilities.");
         break;
 
       case "what the":
